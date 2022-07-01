258f350481c6ceac1cfffafdbeeb09e92480d424
Sbruce3883 <Susan.Bruce@du.edu>
2022-07-01 14:49:41 -0600

    adding remainder of Mapping_GeoJSON_Points logic.js

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,41 +1,41 @@
 // // Add console.log to check to see if our code is working.
-console.log("working");
+// console.log("working");
 
 // // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
+// let map = L.map('mapid').setView([30, 30], 2);
 
 // // Add GeoJSON data.
-let sanFranAirport =
-{"type":"FeatureCollection","features":[{
-    "type":"Feature",
-    "properties":{
-        "id":"3469",
-        "name":"San Francisco International Airport",
-        "city":"San Francisco",
-        "country":"United States",
-        "faa":"SFO",
-        "icao":"KSFO",
-        "alt":"13",
-        "tz-offset":"-8",
-        "dst":"A",
-        "tz":"America/Los_Angeles"},
-        "geometry":{
-            "type":"Point",
-            "coordinates":[-122.375,37.61899948120117]}}
-]};
+// let sanFranAirport =
+// {"type":"FeatureCollection","features":[{
+//     "type":"Feature",
+//     "properties":{
+//         "id":"3469",
+//         "name":"San Francisco International Airport",
+//         "city":"San Francisco",
+//         "country":"United States",
+//         "faa":"SFO",
+//         "icao":"KSFO",
+//         "alt":"13",
+//         "tz-offset":"-8",
+//         "dst":"A",
+//         "tz":"America/Los_Angeles"},
+//         "geometry":{
+//             "type":"Point",
+//             "coordinates":[-122.375,37.61899948120117]}}
+// ]};
 
 
-// // Grabbing our GeoJSON data.
-// // GeoJSON reverse lat-long to long-lat
-L.geoJson(sanFranAirport, {
-  // We turn each feature into a marker on the map.
-  onEachFeature: function(feature, layer) {
-    console.log(layer);
-    layer.bindPopup("<h3>" + "Airport Code: " + feature.properties.faa +
-    "</h3><hr><p>" + feature.properties.name + "</p>");
-  }
+// // // Grabbing our GeoJSON data.
+// // // GeoJSON reverse lat-long to long-lat
+// L.geoJson(sanFranAirport, {
+//   // We turn each feature into a marker on the map.
+//   onEachFeature: function(feature, layer) {
+//     console.log(layer);
+//     layer.bindPopup("<h3>" + "Airport Code: " + feature.properties.faa +
+//     "</h3><hr><p>" + feature.properties.name + "</p>");
+//   }
 
-}).addTo(map);
+// }).addTo(map);
 
 
 // We create the tile layer that will be the background of our map.
@@ -65,20 +65,20 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{
     accessToken: API_KEY
 });
 
-// Create a base layer that holds both maps.
+// // Create a base layer that holds both maps.
 let baseMaps = {
   Street: streets,
   Dark: dark
 };
 
-// Create the map object with center, zoom level and default layer.
+// // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
   center: [30, 30],
   zoom: 2,
   layers: [streets]
 })
 
-// Pass our map layers into our layers control and add the layers control to the map.
+// // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
 // // Then we add our 'graymap' tile layer to the map.
@@ -86,11 +86,14 @@ L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
 // let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
-let airportData = "https://raw.githubusercontent.com/Baylex/Mapping_Earthquakes/main/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/Sbruce3883/Mapping_Earthquakes/main/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
   console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data).addTo(map);
+});
 // Creating a GeoJSON layer with the retrieved data.  Skill Drill 13.5.3 add popups to all markers
 L.geoJson(data,{
   onEachFeature: function(feature, layer) {
@@ -99,4 +102,3 @@ L.geoJson(data,{
     "</h3><hr><p>" + feature.properties.name + "</p>");
   }    
 }).addTo(map);
-});
